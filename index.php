762e82021edbac268c5f479c6f460d79f7d5868b
eyeos <francisco.raya+github@gmail.com>
2016-04-21 20:38:57 +0200

    index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 session_start();
 require 'vendor/autoload.php';
 require_once __DIR__ . "/src/controllers/ControllerLocator.php";
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/page1', 'UserController');
     $r->addRoute('GET', '/page2', 'UserController');
     $r->addRoute('GET', '/page3', 'UserController');
@@ -25,36 +25,37 @@ $httpMethod = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 $contentNegotiation = $_SERVER['HTTP_ACCEPT'];
-$parser = $contentNegotiation == "application/xml"?new XMLParser():new JSONParser();
+$parser = $contentNegotiation == "application/xml" ? new XMLParser() : new JSONParser();
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         header("HTTP/1.0 404 Method Not Found");
-        header("Content-Type: application/".$parser->getName());
-        echo $parser->parse(array('status'=>"404 Method Not Found"));
+        header("Content-Type: application/" . $parser->getName());
+        echo $parser->parse(array('status' => "404 Method Not Found"));
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = implode(",", $routeInfo[1]);
         header("HTTP/1.0 405 Method Not Allowed");
-        header("Allow: ".$allowedMethods);
-        header("Content-Type: application/".$parser->getName());
-        echo $parser->parse(array('status'=>"405 Method Not Allowed", 'allow'=>$allowedMethods));
+        header("Allow: " . $allowedMethods);
+        header("Content-Type: application/" . $parser->getName());
+        echo $parser->parse(array('status' => "405 Method Not Allowed", 'allow' => $allowedMethods));
         break;
     case FastRoute\Dispatcher::FOUND:
         $handler = $routeInfo[1];
         $vars = $routeInfo[2];
+        if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+            $auth = $_SERVER['HTTP_AUTHORIZATION'];
+            $auth = explode(" ", $auth);
 
-        $auth = $_SERVER['HTTP_AUTHORIZATION'];
-        $auth = explode(" ", $auth);
-
-        if ($auth[0] == "Basic") {
-            $credentials = explode(":",base64_decode($auth[1]));
-            $vars['auth_username'] = $credentials[0];
-            $vars['auth_password'] = $credentials[1];
+            if ($auth[0] == "Basic") {
+                $credentials = explode(":", base64_decode($auth[1]));
+                $vars['auth_username'] = $credentials[0];
+                $vars['auth_password'] = $credentials[1];
+            }
         }
 
         $uri = preg_replace("[^/]", "", $uri);
         $elemsUri = explode("/", $uri);
-        $method = strtolower($httpMethod).ucfirst($elemsUri[0]);
+        $method = strtolower($httpMethod) . ucfirst($elemsUri[0]);
         $vars = array_merge($vars, $_POST);
         $controller = ControllerLocator::getInstance($handler, $contentNegotiation, $vars);
         $controller->$method();
