32425a5f57ec21670c18e49208f225e7ea33e2b1
eyeos <francisco.raya+github@gmail.com>
2016-04-21 09:52:25 +0200

    views

--- src/views/templates/forbidden.php
+++ src/views/templates/forbidden.php
@@ -1,10 +1,9 @@
 <?php
 
 header('HTTP/1.0 403 Forbidden');
-echo 'No tienes permisos para ver esta página!';
-
 ?>
 <meta charset="utf-8" />
+<h2>No tienes permisos para ver esta página</h2>
 <form action="/logout" method="post">
     <input type="hidden" name="uri" value="<?php echo $uri?>" />
     <input type="submit" value="Cerrar sesión"
--- src/views/templates/welcome.php
+++ src/views/templates/welcome.php
@@ -4,12 +4,10 @@
 <meta charset="utf-8" />
 <html>
 <body>
-<?php
-echo "PageName: " . $pagename . "\n";
-echo "Hello " . $username;
-?>
-<form action="/logout" method="POST">
-    <input type="hidden" name="uri" value="/<?php $uri?>" />
+<h2>PageName: <?php echo $pagename?></h2>
+<h2>Hello <?php echo $username?></h2>
+<form action="/logout" method="post">
+    <input type="hidden" name="uri" value="/<?php echo $pagename?>" />
     <input type="submit" value="Hacer logout"
            name="Submit" id="frm1_submit"/>
 </form>
